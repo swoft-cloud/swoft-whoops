62adc45e8d7aec7a1a88334154e2affa7b35523b
wujunze <itwujunze@163.com>
2018-05-22 11:03:49 +0800

    init

---
+++ src/WhoopsHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Swoft\Whoops;
+
+use Whoops\Run;
+
+class WhoopsHandler
+{
+
+    private $whoops;
+
+    public function getWhoopsInstance()
+    {
+        if (empty($this->_whoops)) {
+            $this->whoops = new Run();
+        }
+        return $this->whoops;
+    }
+
+}
